28fbba87b15f0d2e62b503d4b0876ba303bb1c83
EugeneWeb <111679803+EugeneWeb@users.noreply.github.com>
2023-08-23 13:33:03 +0300

    Update index-controller.js

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -3,7 +3,7 @@ const createPath = require("../helpers/createPath")
 const getIndex = (req, res) => {
     const title = 'Главная страница'
 
-    res.render(createPath('error'), { title })
+    res.render(createPath('index'), { title })
 }
 
 module.exports = {
